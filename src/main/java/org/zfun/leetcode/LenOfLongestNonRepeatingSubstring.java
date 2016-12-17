7f8d26187b93c750c8e7a054756118c7e67ce82c
Steve Kuo <steve.kuo@makerstudios.com>
2016-12-17 11:41:02 -0800

    A more space efficient solution for LenOfLongestNonRepeatingSubstring

--- src/main/java/org/zfun/leetcode/LenOfLongestNonRepeatingSubstring.java
+++ src/main/java/org/zfun/leetcode/LenOfLongestNonRepeatingSubstring.java
@@ -1,26 +1,31 @@
 package org.zfun.leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LenOfLongestNonRepeatingSubstring {
 
     public int lengthOfLongestSubstring(String s) {
         int maxLen = 0;
         Map<Character,Integer> charPos = new HashMap<>();
+        Set<Character> charToDelete = new HashSet<>();
         for (int pos=0; pos<s.length(); pos++) {
             char c = s.charAt(pos);
             // if char not in map
             Integer lastPos = charPos.get(c);
             if (lastPos != null) {
                 // char is repeated
-                // Create a new Map and loop through charPos and add all entry whose pos > lastPos
-                Map<Character,Integer> newCharPos = new HashMap<>();
+                // Create a new set containing the keys whose whose pos <= lastPos
+                charToDelete.clear();
                 for (Map.Entry<Character, Integer> entry : charPos.entrySet()) {
-                    if (entry.getValue() > lastPos)
-                        newCharPos.put(entry.getKey(), entry.getValue());
+                    if (entry.getValue() <= lastPos)
+                        charToDelete.add(entry.getKey());
                 }
-                charPos = newCharPos;
+                // Loop and remove charToDelete
+                for (Character ch : charToDelete)
+                    charPos.remove(ch);
             }
             charPos.put(c, pos);
             // set maxLen to charPos.size() if it is smaller
