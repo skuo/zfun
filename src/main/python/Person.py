734adbfe2f9895684169371a2f2d3f6adf4030aa
Steve Kuo <steve.kuo@makerstudios.com>
2017-12-12 17:43:27 -0800

    Added examples for mixin and lambda

--- src/main/python/Person.py
+++ src/main/python/Person.py
@@ -110,7 +110,59 @@ class Child1(Left1, Right1):
     def __init__(self):
         print "child1"
         super(Child1, self).__init__()
-            
+
+
+#=====================================================================                
+# Mixin
+# in Python the class hierarchy is defined right to left
+# MRO is resolved from left to right (sibling)
+#=====================================================================                
+class Mixin1(object):
+    def test(self):
+        print "Mixin1"
+    
+    def favor(self):
+        print 'Anyone named Steve'
+
+class Mixin2(object):
+    def test(self):
+        print "Mixin2"
+        
+class BaseClass(object):
+    def whoami(self):
+        print 'BaseClass'
+
+class MyClass(Mixin2, Mixin1, BaseClass):
+    pass
+
+
+#=====================================================================
+# lambda                
+#=====================================================================                
+# https://www.programiz.com/python-programming/anonymous-function
+'''
+http://www.bogotobogo.com/python/python_functions_lambda.php
+
+
+The lambda's general form is :
+
+lambda arg1, arg2, ...argN : expression using arguments
+Function objects returned by running lambda expressions work exactly the same as those created and assigned by defs. 
+However, there are a few differences that make lambda useful in specialized roles:
+
+lambda is an expression, not a statement.
+Because of this, a lambda can appear in places a def is not allowed. For example, places like inside a list literal, 
+or a function call's arguments. As an expression, lambda returns a value that can optionally be assigned a name. 
+In contrast, the def statement always assigns the new function to the name in the header, 
+instead of returning is as a result.
+
+* lambda's body is a single expression, not a block of statements.
+* The lambda's body is similar to what we'd put in a def body's return statement. We simply type the result as 
+  an expression instead of explicitly returning it. Because it is limited to an expression, a lambda is less general 
+  that a def. We can only squeeze design, to limit program nesting. lambda is designed for coding simple functions, 
+  and def handles larger tasks.
+'''
+                    
 #=====================================================================                
 # main 
 #=====================================================================   
@@ -140,3 +192,25 @@ child0 = Child0()
 
 print "\nsuper() last"
 child1 = Child1()
+
+print '\nmixin'
+obj = MyClass()
+obj.test()
+obj.whoami()
+obj.favor()
+
+print '\nlambda'
+double = lambda x: x * 2
+# Output: 10
+print(double(5))
+
+double = lambda x: x * 2
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+# Output: [4, 6, 8, 12]
+print(new_list)
+
+new_list = list(map(lambda x: x * 2 , my_list))
+# Output: [2, 10, 8, 12, 16, 22, 6, 24]
+print(new_list)
+
