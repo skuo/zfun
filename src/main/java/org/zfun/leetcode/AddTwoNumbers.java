83dd1109cbd7ab7303c4304e3059b9b4a0d832a8
Steve Kuo <steve.kuo@makerstudios.com>
2018-01-04 18:09:48 -0800

    Use long instead of int to catch as much integer overflow or underflow as possible

--- src/main/java/org/zfun/leetcode/AddTwoNumbers.java
+++ src/main/java/org/zfun/leetcode/AddTwoNumbers.java
@@ -1,31 +1,48 @@
 package org.zfun.leetcode;
 
+/*
+ You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order 
+ and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ */
 public class AddTwoNumbers {
+    // 1560 / 1562 test cases passed.
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // reconstitute num1
-        int num1 = 0;
-        int multiplicant = 1;
-        while(l1 != null) {
-            num1 = num1 + l1.val * multiplicant; 
-            l1 = l1.next;
+        long num1 = 0;
+        long multiplicant = 1;
+        ListNode curr = l1;
+        while (curr != null) {
+            num1 = num1 + curr.val * multiplicant;
+            curr = curr.next;
             multiplicant *= 10;
-        };
+        }
+        ;
         // reconstitute num2
-        int num2 = 0;
+        long num2 = 0;
         multiplicant = 1;
-        while (l2 != null) {
-            num2 = num2 + l2.val * multiplicant;
-            l2 = l2.next;
+        curr = l2;
+        while (curr != null) {
+            num2 = num2 + curr.val * multiplicant;
+            curr = curr.next;
             multiplicant *= 10;
-        };
+        }
+        ;
         //
-        int sum = num1 + num2;
-        int quotient = sum;
+        long sum = num1 + num2;
+        long quotient = sum;
         ListNode head = null;
         ListNode tail = null;
         while (quotient > 0 || sum == 0) {
-            int remainder = quotient % 10;
-            ListNode node = new ListNode(remainder);
+            long remainder = quotient % 10;
+            ListNode node = new ListNode((int) remainder);
             if (head == null) {
                 head = node;
                 tail = node;
@@ -40,41 +57,77 @@ public class AddTwoNumbers {
         }
         return head;
     }
-    
+
+    public ListNode constructList(int[] inputs) {
+        ListNode head = null, tail = null;
+
+        // inputs is guaranteed to be not empty
+        for (int i = 0; i < inputs.length; i++) {
+            ListNode node = this.new ListNode(inputs[i]);
+            if (head == null) {
+                head = node;
+                tail = node;
+            } else {
+                tail.next = node;
+                tail = tail.next;
+            }
+        }
+
+        return head;
+    }
+
+    public void print(ListNode l) {
+        ListNode curr = l;
+        StringBuilder sb = new StringBuilder("[");
+        int count = 0;
+        while (curr != null) {
+            if (count++ > 0)
+                sb.append(",");
+            sb.append(curr.val);
+            curr = curr.next;
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+
     public static void main(String[] args) {
-        AddTwoNumbers s = new AddTwoNumbers();
+        AddTwoNumbers obj = new AddTwoNumbers();
         //
         ListNode l1 = null;
         ListNode l1Tail = null;
-        ListNode l1Node = s.new ListNode(2);
+        ListNode l1Node = obj.new ListNode(2);
         l1 = l1Node;
         l1Tail = l1Node;
-        l1Node = s.new ListNode(4);
+        l1Node = obj.new ListNode(4);
         l1Tail.next = l1Node;
         l1Tail = l1Node;
-        l1Node = s.new ListNode(3);
+        l1Node = obj.new ListNode(3);
         l1Tail.next = l1Node;
         l1Tail = l1Node;
         //
         ListNode l2 = null;
         ListNode l2Tail = null;
-        ListNode l2Node = s.new ListNode(5);
+        ListNode l2Node = obj.new ListNode(5);
         l2 = l2Node;
         l2Tail = l2Node;
-        l2Node = s.new ListNode(6);
+        l2Node = obj.new ListNode(6);
         l2Tail.next = l2Node;
         l2Tail = l2Node;
-        l2Node = s.new ListNode(4);
+        l2Node = obj.new ListNode(4);
         l2Tail.next = l2Node;
         l2Tail = l2Node;
         //
-        ListNode listNode = s.addTwoNumbers(l1, l2);
-        while(listNode != null) {
-            System.out.print(listNode.val + " ->");
-            listNode = listNode.next;
-        }
+        ListNode listNode = obj.addTwoNumbers(l1, l2);
+        obj.print(listNode);
+
+        int[] inputs1 = new int[] { 9 };
+        l1 = obj.constructList(inputs1);
+        int[] inputs2 = new int[] { 1, 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+        l2 = obj.constructList(inputs2);
+        listNode = obj.addTwoNumbers(l1, l2);
+        obj.print(listNode);
     }
-    
+
     public class ListNode {
         int val;
         ListNode next;
