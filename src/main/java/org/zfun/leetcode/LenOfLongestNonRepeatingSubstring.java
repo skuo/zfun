12c606b85add47603151f712ea79d31bfe7c85ee
Steve Kuo <steve.kuo@makerstudios.com>
2018-01-06 09:16:32 -0800

    Added instructions for LenOfLongestNonRepeatingSubstring.java

--- src/main/java/org/zfun/leetcode/LenOfLongestNonRepeatingSubstring.java
+++ src/main/java/org/zfun/leetcode/LenOfLongestNonRepeatingSubstring.java
@@ -3,23 +3,35 @@ package org.zfun.leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, 
+"pwke" is a subsequence and not a substring.
+ */
 public class LenOfLongestNonRepeatingSubstring {
 
     public int lengthOfLongestSubstring(String s) {
         int maxLen = 0;
         int start = 0;
-        Map<Character,Integer> charPos = new HashMap<>();
-        for (int pos=0; pos<s.length(); pos++) {
+        Map<Character, Integer> charPos = new HashMap<>();
+        for (int pos = 0; pos < s.length(); pos++) {
             char c = s.charAt(pos);
             // if char not in map
             Integer lastPos = charPos.get(c);
             if (lastPos != null) {
                 // char is repeated
-                for (int j=start; j<=lastPos; j++) {
+                for (int j = start; j <= lastPos; j++) {
                     char ch = s.charAt(j);
                     charPos.remove(ch);
                 }
-                start = lastPos+1;
+                start = lastPos + 1;
             }
             charPos.put(c, pos);
             // set maxLen to charPos.size() if it is smaller
