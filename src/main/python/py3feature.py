50d1a506da7b33bb9ef4d172760d4d85d3a20f6f
Steve Kuo <steve.kuo@makerstudios.com>
2018-01-31 09:38:31 -0800

    Added py3feature.py

---
+++ src/main/python/py3feature.py
@@ -0,0 +1,11 @@
+import math
+
+#a, b, *rest = range(10)
+#print ("a={}, b={}, rest={}".format(a,b,rest))
+
+a, b = range(2)
+
+#résumé = "knows Python"
+#π = math.pi
+
+#print("résumé={}, π={}".format(résumé,π))
