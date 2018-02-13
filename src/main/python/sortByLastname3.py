d59d011bec1d34abcf5aed9c9d2952543444f5f3
Steve Kuo <steve@Beyonds-MacBook-Pro.local>
2018-02-12 17:49:35 -0800

    Added python3 version of sortByLastname3.py

---
+++ src/main/python/sortByLastname3.py
@@ -0,0 +1,20 @@
+#=====================================================================                
+# main 
+#=====================================================================   
+names = ["Jonathan Smith", "Bill O'Reilly", "Thomas Sowell Sr.", "Jonathan Smith", "Rose Friedmann"]
+nameSet = set(names)
+
+print (", ".join(str(name) for name in nameSet))
+
+nameTuples = []
+for name in nameSet:
+    words = name.split()
+    nameTuples = nameTuples + [(name, words[1])] 
+
+print (nameTuples)
+
+# sort by lastName
+sortedNameTuples = sorted(nameTuples, key=lambda name: name[1])
+
+print ("-----")
+print (", ".join(name[0] for name in sortedNameTuples))
